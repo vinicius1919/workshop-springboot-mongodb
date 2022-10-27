e9239af601e7cb394a31198a2e192e0b723b71d2
Vinicius Sousa <vinnysousa12@gmail.com>
2022-10-27 16:50:26 -0300

    Consulta simples com query

--- src/main/java/com/viniciussousa/workshopmongo/config/Instantiation.java
+++ src/main/java/com/viniciussousa/workshopmongo/config/Instantiation.java
@@ -40,7 +40,7 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem !", "Vou viajar para SP. Abraços !", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia !", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2021"), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2021"), new AuthorDTO(bob));
